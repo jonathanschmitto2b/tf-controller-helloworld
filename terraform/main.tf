f0472ff814352bd44b7b226ba91338b24a7a4468
jonathanschmitto2b <116752668+jonathanschmitto2b@users.noreply.github.com>
2023-02-22 16:29:28 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,13 @@
 terraform {
-    required_providers {
-        aws = "~> 4.17"
-    }
-    backend "s3" {
-        profile = "planktron"
-        region = "us-east-1"
-        bucket = "planktron-development-ias-tfstates-terraform"
-        key = "infra/dev/"
-    }
+  required_version = ">= 0.12.26"
 }
 
-provider "aws" {
-    profile = "planktron"
-    region = "us-east-1"
-}
-resource "aws_s3_bucket" "plk-bucket-reports" {
-  bucket = "plk-bucket-reports-dev"
-  tags = {
-    Name = "plk-bucket-reports"
-    Project = "Planktron"
-    Env = "dev"
-  }
+variable "subject" {
+   type = string
+   default = "tfctl-rc"
+   description = "Subject to hello"
 }
 
-resource "aws_s3_bucket_acl" "bucket-acl" {
-    bucket = aws_s3_bucket.plk-bucket-reports.id
-    acl = "private"
-}
-
-resource "aws_s3_bucket_public_access_block" "bucket-public-access" {
-  bucket = aws_s3_bucket.plk-bucket-reports.id
-  block_public_acls = true
-  block_public_policy = true
+output "hello_world" {
+  value = "hey hey ya, ${var.subject}!"
 }
